418908f69c7d510126315fbc9c1f70c2d4627f2b
Matheus Lima Ferreira <matheuslf.17@gmail.com>
2021-05-13 00:01:52 -0300

    Initial Commit

---
+++ time-counter/time_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_counterApp.swift
+//  time-counter
+//
+//  Created by Matheus Lima Ferreira on 13/05/21.
+//
+
+import SwiftUI
+
+@main
+struct time_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
